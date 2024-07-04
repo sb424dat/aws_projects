e143e232898d8b084b30e6e838da04422f99596d
sb424dat <frank@4dat.net>
2024-07-04 05:00:48 -0500

    inital commit" inital "

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
+Bash
+Create a .gitignore file in the django-apprunner/myproject directory:
+
+*.log
+*.pot
+*.pyc
+__pycache__
+db.sqlite3
+media
+staticfiles
